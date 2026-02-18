59617c76e49df1594eeea9a4f45eef46b6b45d29
cordeirocode <joaopaulocordeiro_webdev@hotmail.com>
2026-02-18 18:54:09 -0300

    Refactor S3 file size validation to include error handling in handler function

--- index.mjs
+++ index.mjs
@@ -8,28 +8,30 @@ export const handler = async (event) => {
   const Bucket = record.s3.bucket.name;
   const Key = record.s3.object.key;
 
-  const getObjectResult = await s3Client.getObject({
-    Bucket,
-    Key,
-  });
+  try {
+    const getObjectResult = await s3Client.getObject({
+      Bucket,
+      Key,
+    });
 
-  const mega_byte = 1024 * 1024;
+    const mega_byte = 1024 * 1024;
 
-  if (getObjectResult.Contentlength > mega_byte) {
-    log(`File is too large`);
+    if (getObjectResult.Contentlength > mega_byte) {
+      log(`File is too large`);
 
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "File is too large" }),
+      return "ERROR: File is too large";
+    }
+
+    log(`File is OK`);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(event),
     };
+
+    return response;
+  } catch (error) {
+    log(`Error: ${error}`);
+    return "ERROR: " + error.message;
   }
-
-  log(`File is OK`);
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  };
-
-  return response;
 };
